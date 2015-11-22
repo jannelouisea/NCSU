f3b1c8de6e1ba89e9702404c6fea6e5c86e60891
System Administrator <root@Jannes-MacBook-Pro.local>
2015-11-21 22:42:35 -0500

    fixed more stuff

--- Automata/jfave_P2P2/VertexCoverVerification.java
+++ Automata/jfave_P2P2/VertexCoverVerification.java
@@ -33,27 +33,45 @@ public class VertexCoverVerification {
         Scanner input = new Scanner(new File(args[0]));
         Scanner candidates = new Scanner(new File(args[1]));
         Graph graph = new Graph();
+        int key = 0;
         
         // read input from the file to make a graph
-        while (input.hasNextLine()) {
+        while (input.hasNextLine()) { // read through the file
             String line = input.nextLine();
-            Scanner scnLine = new Scanner(line);
-            Integer u = new Integer(scnLine.nextInt());
-            Integer v = new Integer(scnLine.nextInt());
+            System.out.println( line ); // ********
+            Scanner scnLine = new Scanner(line); // create a scanner for each line
+            int u = scnLine.nextInt();
+            int v = scnLine.nextInt();
+            /**
+             Integer u = new Integer(scnLine.nextInt());
+             Integer v = new Integer(scnLine.nextInt());
+             */
+            // check to see if the graph already contains the new vertices
             if (!graph.contains(u)) {
-                graph.addVertex(u);
+                graph.addVertex(key, u);
+                key++;
             }
             if (!graph.contains(v)) {
-                graph.addVertex(v);
+                graph.addVertex(key, v);
+                key++;
             }
-            Edge e = new Edge();
-            e.setStart(u.intValue());
-            e.setEnd(v.intValue());
-            graph.addEdge(e);
+            Edge e = new Edge(); // create an edge
+            // always set the lower value to edge's start value
+            if ( u < v ) {
+                e.setStart(u);
+                e.setEnd(v);
+            } else {
+                e.setStart(v);
+                e.setEnd(u);
+            }
+            graph.addEdge(e); // add edge to the graph's edges list
         }
-        System.out.println( "Vertices: " + graph.printVertices()); // ******
+        System.out.println( "Vertices: " );
+        graph.printVertices(); // ******
         System.out.println( "Edges: " + graph.printEdges() ); // *******
         input.close();
+        
+
         
         // start reading from the candidate solution file
         while (candidates.hasNextLine()) {
